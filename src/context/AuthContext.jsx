90b0b2860a720b6bc665f27b76f691fa07860970
Yassin Amr <yassin.amr.6767@gmail.com>
2026-09-14 11:11:26 +0200

    first step in phase 1, which is project initiliaztion.

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,25 @@
+import React, { createContext, useState, useContext } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  // We will default to Khaled (ID: 1)
+  const [currentUser, setCurrentUser] = useState({ id: 1, name: "Khaled" });
+
+  // A list of our pre-made users from data.sql
+  const availableUsers = [
+    { id: 1, name: "Khaled" },
+    { id: 2, name: "Junior Intern" },
+    { id: 3, name: "Team Lead" },
+  ];
+
+  return (
+    <AuthContext.Provider
+      value={{ currentUser, setCurrentUser, availableUsers }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
